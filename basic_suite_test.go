a1a306f930d452837bd1bd83de1f3ed5764039b2
Vladimir Popov <vladimir.popov@xored.com>
2021-03-02 20:21:29 +0700

    * * * TEST * * *

--- basic_suite_test.go
+++ basic_suite_test.go
@@ -17,18 +17,24 @@
 package test
 
 import (
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/suite"
 
-	"github.com/networkservicemesh/integration-tests/suites/basic"
-	"github.com/networkservicemesh/integration-tests/suites/memory"
+	"github.com/networkservicemesh/integration-k8s-kind/pullkind"
+	"github.com/networkservicemesh/integration-k8s-kind/setup"
 )
 
-func TestRunBasicSuite(t *testing.T) {
-	suite.Run(t, new(basic.Suite))
-}
+func Test(t *testing.T) {
+	suite.Run(t, new(setup.Suite))
 
-func TestRunMemorySuite(t *testing.T) {
-	suite.Run(t, new(memory.Suite))
+	start := time.Now()
+	println("-- START: ", fmt.Sprint(start))
+
+	suite.Run(t, new(pullkind.Suite))
+
+	println("-- END: ", fmt.Sprint(time.Now()))
+	println("-- DURATION: ", fmt.Sprint(time.Since(start)))
 }
