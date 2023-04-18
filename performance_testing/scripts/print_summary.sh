d237a75bde3bfa0fea1157c9889d7a93b3f37a9a
Danil Uzlov <DanilUzlov@yandex.ru>
2023-04-18 13:28:40 +0700

    fix print scripts
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>

--- performance_testing/scripts/print_summary.sh
+++ performance_testing/scripts/print_summary.sh
@@ -7,27 +7,27 @@ echo showing results from "$result_folder"
 function printForFile() {
     filepath=$1
     
-    qps=$(cat "$filepath" | jq .ActualQPS)
-    count=$(cat "$filepath" | jq .DurationHistogram.Count)
-    min=$(cat "$filepath" | jq .DurationHistogram.Min)
-    max=$(cat "$filepath" | jq .DurationHistogram.Max)
-    avg=$(cat "$filepath" | jq .DurationHistogram.Avg)
-    p50=$(cat "$filepath" | jq .DurationHistogram.Percentiles[0].Value)
-    p99=$(cat "$filepath" | jq .DurationHistogram.Percentiles[4].Value)
+    qps=$(< "$filepath" jq .ActualQPS)
+    count=$(< "$filepath" jq .DurationHistogram.Count)
+    min=$(< "$filepath" jq .DurationHistogram.Min)
+    max=$(< "$filepath" jq .DurationHistogram.Max)
+    avg=$(< "$filepath" jq .DurationHistogram.Avg)
+    p50=$(< "$filepath" jq .DurationHistogram.Percentiles[0].Value)
+    p99=$(< "$filepath" jq .DurationHistogram.Percentiles[4].Value)
     
     echo ----------------------
     echo From "$filepath"
     echo QPS: "$qps"
     echo Total queries: "$count"
-    echo Min: $(awk '{print $1 * 1000}' <<<"$min") ms
-    echo Max: $(awk '{print $1 * 1000}' <<<"$max") ms
-    echo Avg: $(awk '{print $1 * 1000}' <<<"$avg") ms
-    echo p50: $(awk '{print $1 * 1000}' <<<"$p50") ms
-    echo p99: $(awk '{print $1 * 1000}' <<<"$p99") ms
+    echo Min: "$(awk '{print $1 * 1000}' <<<"$min")" ms
+    echo Max: "$(awk '{print $1 * 1000}' <<<"$max")" ms
+    echo Avg: "$(awk '{print $1 * 1000}' <<<"$avg")" ms
+    echo p50: "$(awk '{print $1 * 1000}' <<<"$p50")" ms
+    echo p99: "$(awk '{print $1 * 1000}' <<<"$p99")" ms
     echo ----------------------
 }
 
-files="$result_folder"/*
+files="$result_folder/*"
 
 for file in $files
 do
