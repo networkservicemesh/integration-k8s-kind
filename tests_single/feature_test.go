bad8be76e04337d41a3c1b98472bad260694e0ff
Nikita Skrynnik <nikita.skrynnik@xored.com>
2023-06-06 22:12:26 +0700

    delete some things
    Signed-off-by: Nikita Skrynnik <nikita.skrynnik@xored.com>

--- tests_single/feature_test.go
+++ tests_single/feature_test.go
@@ -17,42 +17,12 @@
 package single
 
 import (
-	"flag"
 	"testing"
-
-	"github.com/stretchr/testify/suite"
 
 	"github.com/networkservicemesh/integration-tests/extensions/parallel"
 	"github.com/networkservicemesh/integration-tests/suites/features"
 )
 
-var calicoFlag = flag.Bool("calico", false, "selects calico tests")
-
-// Disabled tests for Calico-vpp:
-// TestMutually_aware_nses - https://github.com/networkservicemesh/integration-k8s-kind/issues/627
-// TestNse_composition     - https://github.com/networkservicemesh/integration-k8s-kind/issues/625
-// TestVl3_basic           - https://github.com/networkservicemesh/integration-k8s-kind/issues/633
-// TestVl3_scale_from_zero - https://github.com/networkservicemesh/integration-k8s-kind/issues/633
-type calicoFeatureSuite struct {
-	features.Suite
-}
-
-func (s *calicoFeatureSuite) BeforeTest(suiteName, testName string) {
-	switch testName {
-	case
-		"TestMutually_aware_nses",
-		"TestNse_composition",
-		"TestVl3_basic",
-		"TestVl3_scale_from_zero":
-		s.T().Skip()
-	}
-	s.Suite.BeforeTest(suiteName, testName)
-}
-
 func TestRunFeatureSuite(t *testing.T) {
-	if *calicoFlag {
-		suite.Run(t, new(calicoFeatureSuite))
-	} else {
-		parallel.Run(t, new(features.Suite))
-	}
+	parallel.Run(t, new(features.Suite))
 }
