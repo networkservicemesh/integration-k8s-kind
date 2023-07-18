bc2ffa881c84e06f1cf732eb4b6abc03242f12d0
Nikita Skrynnik <nikita.skrynnik@xored.com>
2023-07-18 16:26:56 +1100

    add more excluded tests
    Signed-off-by: Nikita Skrynnik <nikita.skrynnik@xored.com>

--- tests_single/heal_test.go
+++ tests_single/heal_test.go
@@ -25,8 +25,23 @@ import (
 
 func TestRunHealSuite(t *testing.T) {
 	parallel.Run(t, new(heal.Suite),
-		"TestRemote_nsm_system_restart_memif_ip",
-		"TestLocal_nsm_system_restart",
 		"TestVl3_nscs_death",
-		"TestVl3_nse_death")
+		"TestVl3_nse_death",
+		"TestLocal_forwarder_death",
+		"TestLocal_forwarder_remote_forwarder",
+		"TestLocal_nsm_system_restart",
+		"TestLocal_nsmgr_local_forwarder_memif",
+		"TestLocal_nsmgr_local_nse_memif",
+		"TestLocal_nsmgr_remote_nsmgr",
+		"TestLocal_nsmgr_restart",
+		"TestRegistry_remote_forwarder",
+		"TestRegistry_remote_nsmgr",
+		"TestRemote_forwarder_death",
+		"TestRemote_forwarder_death_ip",
+		"TestRemote_nsm_system_restart_memif_ip",
+		"TestRemote_nsmgr_death",
+		"TestRemote_nsmgr_remote_endpoint",
+		"TestRemote_nsmgr_restart",
+		"TestRemote_nsmgr_restart_ip",
+	)
 }
