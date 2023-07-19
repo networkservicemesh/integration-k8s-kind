53144d22ece49604aadb2884a5656f8a5c740673
Nikita Skrynnik <nikita.skrynnik@xored.com>
2023-07-19 17:52:25 +1100

    exclude vl3 tests again
    Signed-off-by: Nikita Skrynnik <nikita.skrynnik@xored.com>

--- tests_single/heal_test.go
+++ tests_single/heal_test.go
@@ -43,6 +43,7 @@ func TestRunHealSuite(t *testing.T) {
 		"TestSpire_server_agent_restart",
 		"TestSpire_server_restart",
 		"TestSpire_upgrade",
+		"TestVl3_nscs_death",
 		"TestLocal_nsmgr_local_nse_memif",
 		"TestRegistry_local_endpoint",
 	)
